81d4d33988ffc0175d325017a19285602dfd8a9a
Tyler Churchill <tyjchurchill@gmail.com>
2020-11-18 15:22:10 -0800

    Added comments for components in `code.js`

--- src/components/code.js
+++ src/components/code.js
@@ -12,8 +12,11 @@ const invisibleChar = '\u2800';
 // endregion
 
 function CodeContainer() {
+
+  // DESCRIPTION
   // A component for easily testing input to the `Code` prop by providing
-  // some sample content and a button to toggle through them.
+  // some sample content and a button to toggle through them. Will not be used
+  // in final version of the site; it's for development purposes only
 
   const [index, setIndex] = React.useState(0);
   const strings = [
@@ -43,9 +46,9 @@ function CodeContainer() {
       <Code
         code      = {strings[index]}
         language  = {'language-js'}
-        typeSpeed = {50}
-        backSpeed = {50}
-        spacing   = {50}
+        typeSpeed = {200}
+        backSpeed = {200}
+        spacing   = {200}
       />
 
     </div>
@@ -54,12 +57,24 @@ function CodeContainer() {
 
 function Code(props) {
 
+  // DESCRIPTION
+  // Provides a "typewriter" effect with syntax highlighting.
+  // Uses a declarative approach; you just provide what you want the component
+  // to display and it calculates diffs from the pre-existing content (if any)
+  // and uses that to decide what to edit. The diffs are passed on to `Typer`
+  // components that handle one specific operation code as defined by the diff.
+
+  // IMPLEMENTATION
+  // A ref to the <pre> tag is passed along to `Typer` children so that they
+  // can recalculate highlights for the entire content, rather than just the
+  // diff they are typing.
+
   // PROPS
   //  props.code      - the code after changes are made
   //  props.language  - the language to highlight with
-  //  props.typeSpeed - how many ms to type each character
-  //  props.backSpeed - how many ms to perform a backspace
-  //  props.spacing   - how long to pause in between <Typer /> components
+  //  props.typeSpeed - how many ms it takes to type each character
+  //  props.backSpeed - how many ms it takes to perform a backspace
+  //  props.spacing   - how long in ms to pause in between <Typer /> components
 
   const [prev, setPrev] = useState('');
   const [curr, setCurr] = useState(props.code);
@@ -165,8 +180,22 @@ function Code(props) {
 
 function Typer({code, operation, language, typeSpeed, backSpeed, startDelay, backDelay}) {
 
+  // DESCRIPTION
+  // Executes the typing animation from a diff calculated by a `Code` component
+  // (specified by `code` & `operation` props) and highlighted in the language
+  // specified by the `language` prop.
+
+  // IMPLEMENTATION
+  // Each diff has exactly one associated operation:
+  //   0 : leave text as is (e.g. "def foo(x)" -> "def foo(x)"
+  //   1 : insert text (e.g. "" -> "def foo(x)")
+  //  -1 : delete text (e.g. "def foo(x)" -> "")
+  //
+  // Note that insertion always starts from an empty string and that deletion
+  // always results in an empty string.
+
   // PROPS
-  //  code       - the code to be typed
+  //  code       - the text to be displayed, inserted or deleted
   //  operation  - 0: just display code, 1: insert code, -1: remove code
   //  typeSpeed  - how many ms to type each character
   //  backSpeed  - how many ms to perform a backspace
